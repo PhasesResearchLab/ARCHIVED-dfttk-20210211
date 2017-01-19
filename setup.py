4bbd2a6eec64fa0baedb7b9f7681705e37428186
bocklund <brandonbocklund@gmail.com>
2017-01-18 20:54:34 -0500

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+def readme():
+    with open(os.path.join(module_dir,'README.rst')) as f:
+        return f.read()
+setup(
+    name='PRLWorkflows',
+    packages=['PRLWorkflows'],
+    description='atomate workflows and scripts for the Phases Research Lab',
+    long_description=readme(),
+    version='0.0.1',
+    install_requires=['atomate'],
+    author='Brandon Bocklund',
+    author_email='brandonbocklund@gmail.com',
+    url='https://gitlab.com/bocklund/prl-workflows',
+    )
