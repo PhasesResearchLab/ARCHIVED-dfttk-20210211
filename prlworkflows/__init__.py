efbe7b42b3714090ed3c81e3066f29e5f49340d2
bocklund <brandonbocklund@gmail.com>
2017-09-12 16:25:19 -0400

    ENH/WIP: First attempt at getting stuctures from a database by symmetry and site ratios
    Challenge is that approx 20-60 of the ~200 symmetries are incorrect,
    possibly due to cell shape changing when creating SQS.

--- prlworkflows/__init__.py
+++ prlworkflows/__init__.py
@@ -4,4 +4,4 @@ __version__ = get_versions()['version']
 del get_versions
 
 from prlworkflows.sqs import SQS, enumerate_sqs
-from prlworkflows.sqs_db import SQSDatabase, structures_from_database
+from prlworkflows.sqs_db import SQSDatabase, get_structures_from_database
--- prlworkflows/sqs_db.py
+++ prlworkflows/sqs_db.py
@@ -140,7 +140,7 @@ def SQSDatabase(path):
     return db
 
 
-def structures_from_database(db, subl_model, subl_site_ratios):
+def get_structures_from_database(db, symmetry, subl_model, subl_site_ratios):
     """Returns a list of Structure objects from the db that match the criteria.
 
     The returned list format supports matching SQS to phases that have multiple solution sublattices
@@ -148,19 +148,50 @@ def structures_from_database(db, subl_model, subl_site_ratios):
 
     Parameters
     ----------
-    db : TinyDB
+    db : tinydb.database.Table
         TinyDB database of the SQS database
     symmetry : str
         Spacegroup symbol for a non-mixing endmember as in pymatgen, e.g. 'Pm-3m'.
     subl_model : [[str]]
-        List of strings of species names, in the style of ESPEI `input.json`. This sublattice model
-        can be of higher dimension than the SQS. Outer dimension should be the same length as subl_site_ratios.
+        List of strings of species names. This sublattice model can be of higher dimension than the SQS.
+        Outer dimension should be the same length as subl_site_ratios.
     subl_site_ratios : [[float]]
-        Site ratios of each sublattice. Outer dimension should be the same length as subl_model.
+        Scalar multiple of site ratios of each sublattice. e.g. [1, 2] will match [2, 4] and vice
+        versa. Outer dimension should be the same length as subl_model.
 
     Returns
     -------
-    [SQS]
+    [AbstractSQS]
         Abstract SQSs that match the symmetry and sublattice model.
     """
-    pass
+    def lists_are_multiple(l1, l2):
+        """
+        Returns True if list a is a multiple of b or vice versa
+
+        Parameters
+        ----------
+        l1 : [int]
+        l2 : [int]
+
+        Returns
+        -------
+        bool
+
+        """
+        # can we compare these two lists?
+        if len(l1) != len(l2):
+            return False
+        for a, b in [(l1, l2), (l2, l1)]:  # check the reverse of the lists too
+            # see if all a are perfectly divisible by all b
+            if all([(x % y == 0) for x, y in zip(a, b)]):
+                # see if all have the same multiple
+                if len(set([x/y for x, y in zip(a, b)])) == 1:
+                    return True
+        return False
+
+    from tinydb import where
+    results = db.search((where('symmetry').symbol == symmetry) &
+                        (where('sublattice_site_ratios').test(
+                         lambda x: (lists_are_multiple([sum(subl) for subl in x], subl_site_ratios))))
+              )
+    return results
