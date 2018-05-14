8a1d0187dcbeff4205a803da640003ba726f7b05
bocklund <brandonbocklund@gmail.com>
2018-05-14 15:29:52 -0400

    WIP: phonon calculation for calculation by finite displacements in a single volume

--- prlworkflows/prl_workflows.py
+++ prlworkflows/prl_workflows.py
@@ -1,13 +1,15 @@
 """Custom Phases Research Lab Workflows
 """
-from pymatgen import Structure, Lattice
+
 import numpy as np
 from uuid import uuid4
 
-from fireworks import Workflow
+from fireworks import Workflow, Firework
 from atomate.vasp.config import VASP_CMD, DB_FILE
 
-from prlworkflows.prl_fireworks import PRLOptimizeFW, PRLStaticFW
+from prlworkflows.analysis.phonon import create_supercell_displacements
+from prlworkflows.prl_firetasks import CalculatePhononThermalProperties
+from prlworkflows.prl_fireworks import PRLOptimizeFW, PRLStaticFW, PRLPhononDisplacementFW
 from prlworkflows.input_sets import PRLRelaxSet, PRLStaticSet
 
 
@@ -23,8 +25,9 @@ def get_wf_ev_curve(structure, num_deformations=7, deformation_fraction=0.05, va
     num_deformations: int
     deformation_fraction: float
     vasp_cmd : str
+        Command to run VASP. If None (the default) is passed, the command will be looked up in the FWorker.
     db_file : str
-        Points to the database JSON file
+        Points to the database JSON file. If None (the default) is passed, the path will be looked up in the FWorker.
     name : str
         Name of the workflow
     metadata : dict
@@ -62,4 +65,47 @@ def get_wf_ev_curve(structure, num_deformations=7, deformation_fraction=0.05, va
     wfname = "{}:{}".format(structure.composition.reduced_formula, name)
 
     return Workflow(fws, name=wfname, metadata=metadata)
+
+
+def get_wf_phonon(structure, supercell_matrix, smearing_type='methfessel-paxton', displacement_distance=0.01, vasp_cmd=None, name='Phonon'):
+    """
+    A workflow to calculate the phonon vibration properties for a single volume.
+
+    Parameters
+    ----------
+    structure : pymatgen.Structure
+        Input structure. Should be a unitcell, not a supercell.
+    supercell_matrix : numpy.ndarray
+        3x3 array of the supercell matrix, e.g. [[2,0,0],[0,2,0],[0,0,2]].
+    smearing_type : str
+        It must be one of 'gaussian', 'methfessel-paxton', or 'tetrahedron'. The default
+        is 'methfessel-paxton', which uses a SIGMA of 0.2 and is well suited for metals,
+        but it should not be used for semiconductors or insulators. Using 'tetrahedron'
+        or 'gaussian' gives a SIGMA of 0.05. Any further customizations should use a custom workflow.
+    displacement_distance : float
+        Distance of each displacement. Defaults to 0.01, consistent with phonopy.
+    vasp_cmd : str
+        Command to run VASP. If None (the default) is passed, the command will be looked up in the FWorker.
+    name : str
+        Name of the workflow
+
+    Returns
+    -------
+    Workflow
+        A workflow for a single volume phonon calculation.
+
+    """
+    vasp_cmd = vasp_cmd or VASP_CMD
+
+    supercells, displacement_dicts = create_supercell_displacements(structure, supercell_matrix, displacement_distance=displacement_distance)
+
+    fws = []
+    for sc, disp_dict in zip(supercells, displacement_dicts):
+        fws.append(PRLPhononDisplacementFW(sc, disp_dict, name="phonon_displacement", smearing_type=smearing_type, vasp_cmd=vasp_cmd))
+
+    thermal_props = Firework([CalculatePhononThermalProperties()], parents=fws, name='CalculateThermalProperties',
+                               spec={'unitcell': structure, 'supercell_matrix': supercell_matrix},)
+    fws.append(thermal_props)
+    wfname = "{}:{}".format(structure.composition.reduced_formula, name)
+    return Workflow(fws, name=wfname)
 
