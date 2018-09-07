933502f439c31a482c38423005595034f3e0d597
bocklund <brandonbocklund@gmail.com>
2018-09-07 00:11:25 -0400

    Remove volume optimization wf

--- dfttk/wflows.py
+++ dfttk/wflows.py
@@ -12,53 +12,6 @@ from dfttk.fworks import OptimizeFW, StaticFW, PhononFW
 from dfttk.input_sets import RelaxSet, StaticSet, ForceConstantsSet, RoughStaticSet
 
 
-def get_wf_volume_optimization(structure, num_deformations=7, deformation_fraction=0.1, vasp_cmd=None, db_file=None, vasp_input_set=None, metadata=None, name='EV_Curve', eos="birch_murnaghan"):
-    """
-    Rough E - V curve workflow that can determine approximate minimum volumes
-
-    workflow
-    Parameters
-    ------
-    structure: pymatgen.Structure
-    num_deformations: int
-    deformation_fraction: float
-    vasp_cmd : str
-        Command to run VASP. If None (the default) is passed, the command will be looked up in the FWorker.
-    db_file : str
-        Points to the database JSON file. If None (the default) is passed, the path will be looked up in the FWorker.
-    name : str
-        Name of the workflow
-    metadata : dict
-        Metadata to include
-    eos : str
-        String of the equation of state to use, see ``pymatgen.analysis.eos.EOS.MODELS`` for options.
-    """
-    vasp_cmd = vasp_cmd or VASP_CMD
-    db_file = db_file or DB_FILE
-
-    metadata = metadata or {}
-    tag = metadata.get('tag', '{}'.format(str(uuid4())))
-    if 'tag' not in metadata.keys():
-        metadata['tag'] = tag
-
-    deformations = np.linspace(1-deformation_fraction, 1+deformation_fraction, num_deformations)
-
-    fws = []
-    for i, deformation in enumerate(deformations):
-        struct = structure.copy()
-        struct.scale_lattice(struct.volume*deformation)
-        vis = vasp_input_set or StaticSet(struct)
-        static = StaticFW(structure, name='structure_{}-static'.format(i), vasp_input_set=vis, vasp_cmd=vasp_cmd, db_file=db_file, metadata=metadata)
-        fws.append(static)
-
-    eos_fw = Firework(EOSAnalysis(eos=eos, db_file=db_file, tag=tag), parents=fws[:], name="{}-eos_analysis".format(structure.composition.reduced_formula))
-    fws.append(eos_fw)
-
-    wfname = "{}:{}".format(tag, name)
-
-    return Workflow(fws, name=wfname, metadata=metadata)
-
-
 def get_wf_gibbs(structure, num_deformations=7, deformation_fraction=(-0.05, 0.1),
                  phonon=False, phonon_supercell_matrix=None,
                  t_min=5, t_max=2000, t_step=5,
