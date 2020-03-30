f0405b74811f22316f63f14167e1b204e798b252
Mingqing Liao <liaomq1900127@163.com>
2020-03-29 23:56:39 -0400

    add support for dfttk config -t
    Support for dfttk config -t (test for the configuration of dfttk, currently only support test for configuration of pymatgen)
    
    make clean some tmp files

--- tests/test_EVcheck_QHA.py
+++ tests/test_EVcheck_QHA.py
@@ -99,5 +99,3 @@ def test_eosfit_stderr():
 def test_EVcheck_QHA():
     wf = Firework(EVcheck_QHA(db_file=db_file,vasp_cmd=VASP_CMD,tag="test",metadata={}))
     #print(wf.as_dict())
-
-test_EVcheck_QHA()
